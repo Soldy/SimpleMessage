9093fc92000921e3181e9a379521b80e5a9d6e30
Soldy <4786022+Soldy@users.noreply.github.com>
2020-04-07 13:49:32 +0100

    debugjs fix
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- www/cdn/js/debug.js
+++ www/cdn/js/debug.js
@@ -17,13 +17,13 @@ const debugClass = function(){
         sendDb = false;
     }
     this.send = function(){
+       return send();
+    }
+    let send = function(){
        if(run === true)
            return false;
        if(1 > db.length)
            return false;
-       return send();
-    }
-    let send = function(){
         if(sentDb === false)
             sentDb = JSON.parse(
                JSON.stringify(db)
@@ -31,7 +31,7 @@ const debugClass = function(){
         net.send({
             c : 'debug',
             data:sentDb
-        });
+        },'POST');
     }
     let clear = function(){
        db = [];
@@ -39,6 +39,9 @@ const debugClass = function(){
     let run = false;
     let db = [];
     let sentDb = false;
+    let interval = setInterval(function(){
+        send();
+    },60000);
 }
 
 const debug = new debugClass();
